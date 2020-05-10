c21531bb3b57a6e8829d72c12db3d85ae1d87e55
Themi <Tcm4760@gmail.com>
2020-05-10 18:02:31 -0500

    Added a really crappy "stats box" at the top of the leaderboard page that I stole from codepen and I really hope I remove at some point because its godawful and it clearly isn't meant to be placed in the way I placed it on the page

--- main.py
+++ main.py
@@ -15,4 +15,8 @@ def leaderboard_page(code_type='ALL'):
     return render_template("leaderboard.html",
                            tops=utils.test_data_sets.tops_data,
                            code_type=code_type,
-                           enumerate=enumerate)
+                           enumerate=enumerate,
+                           amount_found=5,
+                           amount_created=10,
+                           total_scan_amount=7,
+                           users_on_board=len(utils.test_data_sets.tops_data))
