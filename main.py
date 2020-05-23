0eabcc2047ea5d3dd25d607bbcb2dea6f4b05134
Themi <Tcm4760@gmail.com>
2020-05-22 22:53:52 -0500

    -added num_utils.py -added extra leaderboard features -added a favicon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 import utils.test_data_sets
+import utils.num_utils
 
 app = Flask(__name__)
 
@@ -9,9 +11,15 @@ def index_page():
     return render_template("index.html")
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'noggin.ico', mimetype='image/vnd.microsoft.icon')
+
+
 @app.route('/leaderboard/<code_type>')
 @app.route('/leaderboard')
-def leaderboard_page(code_type='ALL'):
+def leaderboard_page(code_type: str = 'ALL'):
     return render_template("leaderboard.html",
                            tops=utils.test_data_sets.tops_data,
                            code_type=code_type,
@@ -19,4 +27,6 @@ def leaderboard_page(code_type='ALL'):
                            amount_found=5,
                            amount_created=10,
                            total_scan_amount=7,
-                           users_on_board=len(utils.test_data_sets.tops_data))
+                           users_on_board=len(utils.test_data_sets.tops_data),
+                           ordinal=utils.num_utils.ordinal,
+                           user_placement=7)
