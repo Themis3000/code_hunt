76efed713b3021d15bbdcbda5907c949354d38d8
Themi <Tcm4760@gmail.com>
2020-05-26 18:19:15 -0500

    -Added to index.html with a little info -Added a request limiter, possibly by the end if everything is finished and running smooth will make some fancy dynamic limit rates

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from flask import Flask, render_template, send_from_directory
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import os
 import utils.test_data_sets
 import utils.num_utils
 
 app = Flask(__name__)
 
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per hour", "1 per second"])
+
 
 @app.route('/')
+@limiter.exempt
 def index_page():
     return render_template("index.html")
 
 
 @app.route('/favicon.ico')
+@limiter.exempt
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/leaderboard')
+@limiter.limit("10/minute")
 def leaderboard_page():
     return render_template("leaderboard.html",
                            tops=utils.test_data_sets.tops_data,
@@ -31,6 +41,7 @@ def leaderboard_page():
 
 
 @app.route('/code/<code_id>')
+@limiter.limit("10/minute")
 def code_page(code_id: str):
     return render_template("code.html",
                            code_data=utils.test_data_sets.code_data,
