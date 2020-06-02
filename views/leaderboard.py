7b620af3572f3f14e9d0aaf03a316851740c2cfe
Themi <Tcm4760@gmail.com>
2020-06-02 16:04:00 -0500

    -updated leaderboard to always show a sensible and same amount option of pages at the bottom -leaderboard.html no longer displays page selector is there is only 1 page

--- views/leaderboard.py
+++ views/leaderboard.py
@@ -12,15 +12,22 @@ class LeaderboardView(FlaskView):
     def index(self):
         page = int(request.args.get('page', '1'))
         # redirects user if supplied page is not in the page range
-        if not 1 <= page <= pages:
+        if page not in range(1, pages+1):
             return redirect(url_for("LeaderboardView:index"))
 
-        # shifts the display_page_range so that it dosen't show the page 0 or pages+1
-        modifier = -1
-        if page == 1:
-            modifier += 1
-        elif page == pages:
-            modifier += -1
+        prev_pages = 1
+        next_pages = 2
+        # generates the page range based on the amount of desired previous pages and next pages for there to be around
+        # the selected page in a best case, will always show prev_pages + next_pages + 1 if possible, otherwise will
+        # scale down the amount displayed. Yes I know this is probably done poorly, but it works
+        if page + next_pages >= pages:
+            end_page = page + (pages - page)
+            start_page = page - ((next_pages - (pages - page)) + prev_pages)
+        else:
+            end_page = page + next_pages if page != 1 else page + next_pages + 1
+            start_page = page - prev_pages
+        start_page = start_page if start_page >= 1 else 1
+        page_range = range(start_page, end_page + 1)
 
         return render_template("leaderboard.html",
                                tops=utils.test_data_sets.tops_data,
@@ -33,4 +40,4 @@ class LeaderboardView(FlaskView):
                                user_placement=7,
                                page=page,
                                pages=pages,
-                               display_page_range=range(page + modifier, page + 3 + modifier))
+                               display_page_range=page_range)
